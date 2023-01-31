f3061e129ae5279d92191358a29f0928f6a5df7d
Alexander Opryshko <alexopryshko@yandex.ru>
2023-01-31 18:03:37 +0300

    codestyle

--- examples/simple.py
+++ examples/simple.py
@@ -2,17 +2,21 @@ from shopback_sdk.products import ClientSDK, ProductData
 
 c = ClientSDK("session_id")
 
-attachment_ids = c.upload_urls([
-    "https://images.ctfassets.net/hrltx12pl8hq/3j5RylRv1ZdswxcBaMi0y7/b84fa97296bd2350db6ea194c0dce7db/Music_Icon.jpg"
-])
+attachment_ids = c.upload_urls(
+    [
+        "https://images.ctfassets.net/hrltx12pl8hq/3j5RylRv1ZdswxcBaMi0y7/b84fa97296bd2350db6ea194c0dce7db/Music_Icon.jpg"
+    ]
+)
 
-product_id = c.upload_product_and_return_id(ProductData(
-    project_id=273,
-    category_name="test",
-    name="test",
-    description="test",
-    price=20,
-    attachment_ids=attachment_ids,
-    external_id="test_external_id"
-))
+product_id = c.upload_product_and_return_id(
+    ProductData(
+        project_id=273,
+        category_name="test",
+        name="test",
+        description="test",
+        price=20,
+        attachment_ids=attachment_ids,
+        external_id="test_external_id",
+    )
+)
 print(product_id)
--- shopback_sdk/old_client_sdk.py
+++ shopback_sdk/old_client_sdk.py
@@ -1,10 +1,10 @@
-import os
-from dataclasses import dataclass, field
 from typing import Optional
 
-import requests
+import os
+from dataclasses import dataclass, field
 
-from gql import gql, Client
+import requests
+from gql import Client, gql
 from gql.transport.requests import RequestsHTTPTransport
 
 
@@ -55,9 +55,7 @@ class OldClientSDK:
     def __init__(self, session_id: str, url: Optional[str] = None) -> None:
         self.url = url or self.DEFAULT_URL
         self.session_id = session_id
-        t = RequestsHTTPTransport(
-            self.graphql_url, headers=self.session_cookies
-        )
+        t = RequestsHTTPTransport(self.graphql_url, headers=self.session_cookies)
         self.client = Client(transport=t, fetch_schema_from_transport=True)
 
     @property
@@ -75,12 +73,9 @@ class OldClientSDK:
     def upload_files(self, paths: list[str]) -> list[int]:
         res = []
         files = [
-            ("file", (os.path.basename(item), open(item, mode="rb"), "image/*"))
-            for item in paths
+            ("file", (os.path.basename(item), open(item, mode="rb"), "image/*")) for item in paths
         ]
-        response = requests.post(
-            self.upload_url, headers=self.session_cookies, files=files
-        )
+        response = requests.post(self.upload_url, headers=self.session_cookies, files=files)
         if response.status_code == 200:
             data = response.json()["data"]
             res += [item["aid"] for item in data]
@@ -95,9 +90,7 @@ class OldClientSDK:
                 continue
             name = url.split("/")[-1]
             files = [("file", (name, r.content, "image/*"))]
-            response = requests.post(
-                self.upload_url, headers=self.session_cookies, files=files
-            )
+            response = requests.post(self.upload_url, headers=self.session_cookies, files=files)
             if response.status_code == 200:
                 data = response.json()["data"]
                 res += [item["aid"] for item in data]
@@ -252,9 +245,7 @@ class OldClientSDK:
             "project": str(category.project_id),
             "name": category.name,
             "externalId": category.external_id,
-            "attachment": str(category.attachment)
-            if category.attachment
-            else "",
+            "attachment": str(category.attachment) if category.attachment else "",
             "parentExternalId": category.parent_external_id or "",
         }
         result = self.client.execute(query, variable_values=params)
